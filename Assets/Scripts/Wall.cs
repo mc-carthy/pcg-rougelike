e9adcd2c9d77f8b6c8e37849e7c35c2637460107
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-05 22:16:40 +0100

    Add health pick-ups

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Random = UnityEngine.Random;
 
-public class Wall : MonoBehaviour
-{
+public class Wall : MonoBehaviour {
 	public Sprite dmgSprite;					//Alternate sprite to display after Wall has been attacked by player.
 	public int hp = 3;							//hit points for the wall.
+	public GameObject[] foodTiles;
 
 	private SpriteRenderer spriteRenderer;		//Store a component reference to the attached SpriteRenderer.
 
@@ -16,18 +17,19 @@ public class Wall : MonoBehaviour
 	
 	
 	//DamageWall is called when the player attacks a wall.
-	public void DamageWall (int loss)
-	{
+	public void DamageWall (int loss) {
 		
-		//Set spriteRenderer to the damaged wall sprite.
 		spriteRenderer.sprite = dmgSprite;
-		
-		//Subtract loss from hit point total.
 		hp -= loss;
 		
 		//If hit points are less than or equal to zero:
-		if(hp <= 0)
-			//Disable the gameObject.
+		if (hp <= 0) {
+			if (Random.Range (0, 5) == 1) {
+				GameObject toInstantiate = foodTiles [Random.Range (0, foodTiles.Length)];
+				GameObject instance = Instantiate (toInstantiate, new Vector3 (transform.position.x, transform.position.y, 0f), Quaternion.identity) as GameObject;
+				instance.transform.SetParent (transform.parent);
+			}
 			gameObject.SetActive (false);
+		}
 	}
 }
